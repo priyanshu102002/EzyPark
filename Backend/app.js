63006cd9b70844f1c5a0136ab25395252e978212
priyanshukr449 <priyanshukr449@gmail.com>
2024-03-17 09:24:12 +0530

    some basic changes

--- Backend/app.js
+++ Backend/app.js
@@ -27,7 +27,12 @@ app.use(cors())
 // Connect Database
 connectDB();
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname,"Frontend/build")));
+
+app.get("*", (req, res) => { 
+    res.sendFile(path.join(__dirname, 'Frontend', 'build', 'index.html'))
+})
 
 app.get('/', isLoggedIn, async (req, res) => {
     res.json({ message: 'Hello world!'})
@@ -40,10 +45,7 @@ app.use("/booking", bookingRouter)
 app.use("/space", spaceRouter)
 app.use("/review", reviewRouter)
 
-app.use(express.static(path.join(__dirname,"Frontend/build")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'Frontend', 'build', 'index.html'))
-})
+
 
 // Error handler
 
